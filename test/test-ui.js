8eddd8ca107e02b46e6cf6e61707efbffbd2d9f8
tripu <tripu@tripu.info>
2015-06-21 02:56:03 +0900

    Work on initial version

--- test/test-ui.js
+++ test/test-ui.js
@@ -6,26 +6,34 @@ var fs = require('fs'),
 
 var app = new express();
 
-// console.dir(__dirname + '/json');
 app.use(express.static(__dirname + '/json'));
 
-// app.get('/*.json', function(req, res) {
-// });
+var static = function(req, res) {
+  fs.readFile(__dirname + '/' + req.originalUrl, function (err, data) {
+    if (err) {
+      res.status(404).send('Oops!');
+    } else {
+      res.sendFile(__dirname + '/' + req.originalUrl);
+    }
+  });
+};
 
-app.get('/*', function(req, res) {
+app.get('/style.css', static);
+app.get('/favicon.ico', static);
+app.get(/.+\.json/i, static);
+
+app.get(/(index\.html)?/i, function(req, res) {
   var fileName = req.originalUrl;
   if (/\/$/.test(fileName)) {
     fileName += '/index.html';
   }
   fs.readFile(__dirname + '/' + fileName, function (err, data) {
     if (err) {
-      if ('ENOENT' !== err.code || !(/favicon\.ico$/i.test(err.path))) {
-        res.status(404).send('Oops!');
-      }
+      res.status(404).send('Oops!');
     } else {
       Performs.filter(String(data), function (result) {
         res.status(200).send(result);
-      }, req);
+      }, req, {prependJSON: true});
     }
   });
 });
